e9114476bf4932cd1c809b94460f80a8c28b540e
leticiabizaglio <leticia.bizaglio@aluno.senai.br>
2024-05-06 16:59:30 -0300

    porta q está rodando

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(express.json());
 
@@ -26,7 +26,7 @@ app.get('/jogadores', async (req, res) => {
         })
     } catch (error) {
         console.log("Erro ao obter jogadores: " + error);
-        res.sendStatus(500).send("Erro ao obter todos os jogadores");
+        res.status(500).send("Erro ao obter todos os jogadores");
     }
 });
 
@@ -42,7 +42,7 @@ app.get('/jogadores/:id', async (req, res) => {
         }
     } catch (error) {
         console.log("Erro ao obter jogador: " + error);
-        res.sendStatus(500).send("Erro ao obter o jogador");
+        res.status(500).send("Erro ao obter o jogador");
     }
 });
 
@@ -59,58 +59,58 @@ app.get('/jogadores/nome/:nome', async (req, res) => {
         }
     } catch (error) {
         console.log("Erro ao obter jogador: " + error);
-        res.sendStatus(500).send("Erro ao obter o jogador");
+        res.status(500).send("Erro ao obter o jogador");
     }
 });
 
 
-//Rota para adicionar um novo jogador
+// Rota para adicionar um novo jogador
 app.post('/jogadores', async (req, res) => {
     try {
-        const {nome, idade, velocidade, habilidade, posicao, time} = req.body;
+        const { nome, idade, velocidade, habilidade, posicao, time } = req.body;
 
         await pool.query('INSERT INTO jogadores (nome, idade, velocidade, habilidade, posicao, time) VALUES ($1, $2, $3, $4, $5, $6)', [nome, idade, velocidade, habilidade, posicao, time]);
-        res.sendStatus(201).send("Jogador criado com sucesso");
-
+        res.status(201).send({ message: "Jogador criado com sucesso" });
     } catch (error) {
         console.log("Erro ao criar jogador: " + error);
-        res.sendStatus(500).send("Erro ao criar o jogador");
+        res.status(500).send("Erro ao criar o jogador");
     }
 });
 
 
 // Rota para atualizar um jogador
 app.put('/jogadores/:id', async (req, res) => {
-    try{
+    try {
         const { id } = req.params;
         const { nome, idade, velocidade, habilidade, posicao, time } = req.body;
 
         await pool.query('UPDATE jogadores SET nome = $1, idade = $2, velocidade = $3, habilidade = $4, posicao = $5, time = $6 WHERE id = $7', [nome, idade, velocidade, habilidade, posicao, time, id]);
-        res.sendStatus(200).send("Jogador atualizado com sucesso");
+        res.status(201).send({ message: "Jogador atualizado com sucesso" });
     } catch (error) {
         console.log("Erro ao atualizar jogador: " + error);
-        res.sendStatus(500).send("Erro ao atualizar o jogador");
+
+        res.status(500).send("Erro ao atualizar o jogador");
     }
 });
-
 
 // Rota para deletar um jogador
 app.delete('/jogadores/:id', async (req, res) => {
-    try{
+    try {
         const { id } = req.params;
 
         await pool.query('DELETE FROM jogadores WHERE id = $1', [id]);
-        res.sendStatus(200).send("Jogador deletado com sucesso");
+        res.status(200).send({ message: "Jogador deletado com sucesso" });
     } catch (error) {
         console.log("Erro ao deletar jogador: " + error);
-        res.sendStatus(500).send("Erro ao deletar o jogador");
+        res.status(500).send("Erro ao deletar o jogador");
     }
 });
+
 
 
 // Rota de teste
 app.get('/', (req, res) => {
-    res.send({message: 'Servidor funcionando!'});
+    res.send({ message: 'Servidor funcionando!' });
 });
 
 
